dfe4a394574d235c5375db04cff47cdc032e89e7
yuvro <23ymultani@athenian.org>
2022-02-27 18:05:39 -0800

    entrepreneurial mindset

--- src/main/java/KeyCounter.java
+++ src/main/java/KeyCounter.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -6,51 +8,52 @@ import static java.awt.event.KeyEvent.*;
 
 public class KeyCounter {
     JFrame frame;
-    JButton getCount;
-    JTextField field1;
-    JTextField field2;
-    int count;
-
+    JButton button1;
+    JTextField field1, field2;
+    int count = 0;
     public KeyCounter() {
+
         frame = new JFrame("Key Counter");
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         field1 = new JTextField();
-        getCount = new JButton("Get press count");
         field2 = new JTextField();
+        button1 = new JButton("Check Press Count");
+
 
         frame.add(field1);
-        frame.add(getCount);
+        frame.add(button1);
         frame.add(field2);
-        frame.pack();
-        frame.setVisible(true);
 
-        getCount.addKeyListener(new KeyListener() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                count++;
-            }
-
+        field1.addKeyListener(new KeyListener() {
             @Override
             public void keyTyped(KeyEvent e) {
 
             }
 
-
+            @Override
+            public void keyPressed(KeyEvent e) {
+              count ++;
+            }
 
             @Override
             public void keyReleased(KeyEvent e) {
 
             }
-
-
-        }
-                field2.setText(count +""));
+        });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                field2.setText(count + "");
                 count = 0;
+            }
+        });
 
 
-
+        frame.pack();
+        frame.setVisible(true);
 
     }
 
